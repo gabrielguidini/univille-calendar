da0637014c4d81f0611da920b1d98183600730be
gabrielguidini <gabrielsguidini@gmail.com>
2024-05-11 22:03:22 -0300

    initial project commit

---
+++ src/main/java/br/com/univillecalendar/UnivilleCalendarApplication.java
@@ -0,0 +1,13 @@
+package br.com.univillecalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivilleCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivilleCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/univillecalendar/UnivilleCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.univillecalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivilleCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
