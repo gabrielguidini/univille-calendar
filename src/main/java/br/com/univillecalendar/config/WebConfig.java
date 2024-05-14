1554d249f5d1caecc00b4ca69fec9dc7de2db812
gabrielguidini <gabrielsguidini@gmail.com>
2024-05-13 22:03:48 -0300

    adjust

--- src/main/java/br/com/univillecalendar/config/WebConfig.java
+++ src/main/java/br/com/univillecalendar/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
