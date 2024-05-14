9f5ae70407428f150ea051e744cd246eaa831a2f
gabrielguidini <gabrielsguidini@gmail.com>
2024-05-13 21:47:28 -0300

    feat: adding cors configuration

--- src/main/java/br/com/univillecalendar/controller/TeacherController.java
+++ src/main/java/br/com/univillecalendar/controller/TeacherController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@RestController("/teacher")
+@RestController()
 @Slf4j
 public class TeacherController {
 
