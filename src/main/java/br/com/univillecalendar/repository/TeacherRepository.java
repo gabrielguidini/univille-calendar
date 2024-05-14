4584cdfb959d4e502802f026707319c6408547a5
gabrielguidini <gabrielsguidini@gmail.com>
2024-05-13 21:16:08 -0300

    feat: finishing update

--- src/main/java/br/com/univillecalendar/repository/TeacherRepository.java
+++ src/main/java/br/com/univillecalendar/repository/TeacherRepository.java
@@ -2,7 +2,6 @@ package br.com.univillecalendar.repository;
 
 import br.com.univillecalendar.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
--- src/main/java/br/com/univillecalendar/service/SubjectService.java
+++ src/main/java/br/com/univillecalendar/service/SubjectService.java
@@ -1,4 +1,13 @@
 package br.com.univillecalendar.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SubjectService {
+
+    /*todo: make the rest of the CRUD developing around the business rules and the SOLID's principle.
+     * tip: the main methods that create, delete, read etc, normally is up in the file. create -> delete ............
+     *  and finishing the methods that implements the other main methods
+     * */
+
 }
