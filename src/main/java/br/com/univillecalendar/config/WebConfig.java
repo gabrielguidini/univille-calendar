9843a6959ce6661c2ae1ff246dac2243d0a956e0
gabrielguidini <gabrielsguidini@gmail.com>
2024-05-15 12:40:26 -0300

    adjusting cors config again

--- src/main/java/br/com/univillecalendar/config/WebConfig.java
+++ src/main/java/br/com/univillecalendar/config/WebConfig.java
@@ -14,9 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://localhost:8080")
+                .allowedOriginPatterns("http://localhost:3000", "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowCredentials(true)
+                .allowedHeaders("*") // or specify specific headers, e.g., "Content-Type", "Authorization"
+                .exposedHeaders("Content-Type", "Authorization") // or specify specific headers
                 .maxAge(3600);
     }
 }
