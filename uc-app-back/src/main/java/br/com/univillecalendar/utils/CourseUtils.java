f6fa13a9a584f180a354ca479310e1a0bda4b2ac
gabrielguidini <gabrielsguidini@gmail.com>
2024-05-15 17:40:39 -0300

    adding course endpoint

---
+++ uc-app-back/src/main/java/br/com/univillecalendar/utils/CourseUtils.java
@@ -0,0 +1,32 @@
+package br.com.univillecalendar.utils;
+
+import br.com.univillecalendar.dto.CourseDto;
+import br.com.univillecalendar.dto.TeacherDto;
+import br.com.univillecalendar.model.Course;
+import br.com.univillecalendar.model.Teacher;
+import lombok.experimental.UtilityClass;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@UtilityClass
+public class CourseUtils {
+
+    public CourseDto convertEntityToDto(Course course) {
+
+        return CourseDto.builder()
+                .courseName(course.getCourseName())
+                .courseType(course.getCourseType())
+                .subjects(course.getSubjects().stream().map(SubjectUtils::convertEntityToDto).collect(Collectors.toList()))
+                .build();
+    }
+
+    public Course convertDtoToEntity(CourseDto course) {
+
+        return Course.builder()
+                .courseName(course.getCourseName())
+                .courseType(course.getCourseType())
+                .subjects(course.getSubjects().stream().map(SubjectUtils::convertDtoToEntity).collect(Collectors.toList()))
+                .build();
+    }
+}
