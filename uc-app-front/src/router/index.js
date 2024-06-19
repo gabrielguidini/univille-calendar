16cc93f4fa26a6a182ae44450253e28b9a4f3885
ivan <ivan.trapp@univille.br>
2024-06-19 16:06:17 -0300

    asdasd

--- uc-app-front/src/router/index.js
+++ uc-app-front/src/router/index.js
@@ -9,13 +9,13 @@ import LoginView from "@/views/LoginView.vue";
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
+		// {
+		// 	path: "/",
+		// 	name: "login",
+		// 	component: LoginView,
+		// },
 		{
 			path: "/",
-			name: "login",
-			component: LoginView,
-		},
-		{
-			path: "/home",
 			name: "home",
 			component: HomeView,
 		},
--- uc-app-front/src/views/HomeView.vue
+++ uc-app-front/src/views/HomeView.vue
@@ -1,8 +1,16 @@
 <script setup>
+import VSchedules from "@/components/VSchedules.vue";
 import VHome from "../components/VHome.vue";
 </script>
 <template>
-  <h1>inicio</h1>
+	<v-main>
+		<v-container class="d-flex flex-column ga-5">
+			<h1>Início</h1>
+			<v-row>
+				<VSchedules />
+			</v-row>
+		</v-container>
+	</v-main>
 </template>
 
 <style></style>
